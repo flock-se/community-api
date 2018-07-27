f734511c62933d655475123c890b41fbf111dc7d
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2018-07-27 09:08:38 +0200

    ui working

--- src/main/kotlin/com/flock/community/api/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/flock/community/api/config/WebSecurityConfig.kt
@@ -1,28 +1,17 @@
 package com.flock.community.api.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.flock.community.api.model.User
 import com.flock.community.api.repositories.UserRepository
-import com.flock.community.api.authorities.UserAuthorities
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.context.SecurityContextHolder
 
-
-private val TOKEN_HEADER = "Authorization"
-
-val objectMapper = ObjectMapper()
 
 @Configuration
 @EnableOAuth2Sso
@@ -30,18 +19,24 @@ val objectMapper = ObjectMapper()
 //@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 open class SecurityConfig() : WebSecurityConfigurerAdapter() {
 
-    @Throws(Exception::class)
+    override fun configure(web: WebSecurity) {
+        web
+                .ignoring()
+                .antMatchers() // #3
+    }
     override fun configure(http: HttpSecurity) {
 
         http
-                .antMatcher("/**")
                 .authorizeRequests()
-                    .antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
+                    .antMatchers("/login**", "/webjars/**", "/error**", "/resources/**", "/frontend**", "/VAADIN**").permitAll()
                     .antMatchers("/_ah/**").permitAll()
                     .antMatchers("/frontend/**").permitAll()
                     .mvcMatchers("/api/register").permitAll()
                     .antMatchers("/api/buckaroo/**").permitAll()
-                    .anyRequest().authenticated()
+                    .antMatchers("/api/**", "/ui/**").fullyAuthenticated()
+                .and()
+                    .csrf().disable();
+
     }
 
     @Bean
