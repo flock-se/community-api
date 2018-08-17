f13b2d2568b92aac1f0e4b2e7e67988d7ac6b9d3
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2018-08-17 12:43:29 +0200

    Fix integration tests

--- src/main/kotlin/com/flock/community/api/controllers/DonationsController.kt
+++ src/main/kotlin/com/flock/community/api/controllers/DonationsController.kt
@@ -1,11 +1,13 @@
 package com.flock.community.api.controllers
 
 import com.flock.community.api.model.Donation
+import com.flock.community.api.model.Frequency
 import com.flock.community.api.repositories.DonationRepository
 import com.flock.community.api.service.BuckarooService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import community.flock.eco.feature.member.model.Member
+import community.flock.eco.feature.payment.model.PaymentTransaction
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 
 @RestController
@@ -15,9 +17,43 @@ open class DonationsController(
         private val donationRepository: DonationRepository) {
 
 
+    data class Donate(
+
+            val amount: Double,
+            val issuer: String,
+
+            val member: Member? = null,
+
+            val newsletter: Boolean,
+            val agreeOnTerms: Boolean
+    )
+
     @GetMapping
     fun donate(): List<Donation>? {
         return donationRepository.findAll().toList()
+    }
+
+    @PostMapping("/donate")
+    fun donate(@RequestBody donate: Donate): String {
+
+        val buckarooTransaction = buckarooService.createTransaction(donate.amount, "Donate", donate.issuer)
+
+        val transaction = PaymentTransaction(
+                amount = donate.amount,
+                reference = buckarooTransaction.reference
+        )
+
+        val donation = Donation(
+                date = Date(),
+                amount = donate.amount,
+                frequency = Frequency.ONCE,
+                member = donate.member,
+                transactions = setOf(transaction)
+        )
+
+        donationRepository.save(donation)
+
+        return buckarooTransaction.redirectUrl
     }
 
 }
